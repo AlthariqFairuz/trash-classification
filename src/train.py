5c10d04c061a35fecb382614a8aea879969fb376
Althariq Fairuz <112919962+AlthariqFairuz@users.noreply.github.com>
2025-01-10 14:15:35 +0700

    chore: change number of epochs and patience

--- src/train.py
+++ src/train.py
@@ -69,5 +69,5 @@ if __name__ == "__main__":
     optimizer = optim.Adam(model.parameters(), lr=0.0001)
     
     train_loader, val_loader, _ = load_data()
-    train_model(model, train_loader, val_loader, criterion, optimizer, epochs=50, patience=5)
+    train_model(model, train_loader, val_loader, criterion, optimizer, epochs=10, patience=3)
     wandb.finish()
